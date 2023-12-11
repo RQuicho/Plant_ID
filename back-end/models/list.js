4ddecea75dd9d528065f25e131a1b07e95c2d469
RQuicho <raymondquicho@gmail.com>
2023-12-11 00:30:27 -0800

    creating list model

--- back-end/models/list.js
+++ back-end/models/list.js
@@ -56,7 +56,43 @@ class List {
   }
 
   // UPDATE
-  
+  static async update(name, data) {
+    const {setCols, values} = sqlForPartialUpdate(
+      data,
+      {
+        name: 'name',
+        description: 'description',
+      });
+    const nameVarIdx = "$" + (values.length +1);
 
+    const querySql = `UPDATE lists
+                      SET ${setCols}
+                      WHERE name=${nameVarIdx}
+                      RETURNING name, description`;
+    const result = await db.query(querySql, [...values, name]);
 
+    const list = result.rows[0];
+    if (!list) {
+      throw new NotFoundError(`No list: ${name}`);
+    }
+
+    return list;
+  }
+
+  // REMOVE
+  static async remove(name) {
+    let result = await db.query(
+      `DELETE
+       FROM lists
+       WHERE name = $1
+       RETURNING name`,
+      [name]
+    );
+    const list = result.rows[0];
+    if (!list) {
+      throw new NotFoundError(`No list: ${name}`);
+    }
+  }
 }
+
+module.exports = List;
