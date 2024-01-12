b7921b0ed583e512e22cea262d12f6a0a25419aa
RQuicho <raymondquicho@gmail.com>
2024-01-12 12:31:18 -0800

    updating config.js on back end

--- back-end/config.js
+++ back-end/config.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret";
+// const SECRET_KEY = process.env.SECRET_KEY || "secret";
 // const PORT = process.env.PORT || 3001;
-const PORT = process.env.DATABASE_URL;
+const SECRET_KEY = process.env.SECRET_KEY || "secret";
+const PORT = process.env.DATABASE_URL || 3001;
 
 let DB_NAME;
 
